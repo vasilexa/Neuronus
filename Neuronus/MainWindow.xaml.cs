962c2f5786bb6bc60520398dfb2d281c73388b32
Medoo48 <artem_1889@mail.ru>
2019-09-07 19:07:35 +0300

    Написал базовую обертку

--- Neuronus/MainWindow.xaml.cs
+++ Neuronus/MainWindow.xaml.cs
@@ -1,16 +1,10 @@
 ﻿using System;
-using System.Linq;
-using System.Text;
 using System.Windows;
-using System.Windows.Data;
+using Microsoft.Win32;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Shapes;
 using System.Threading.Tasks;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Navigation;
-using System.Collections.Generic;
 using System.Windows.Media.Imaging;
 
 
@@ -18,9 +12,53 @@ namespace Neuronus
 {
     public partial class MainWindow : Window
     {
-        public MainWindow()
-        { InitializeComponent(); }
+        public MainWindow() { InitializeComponent(); }
 
-        void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) => DragMove();
+        Point p1, p2;
+        void Canvas_MouseMove(object sender, MouseEventArgs e)
+        {//Рисует только тогда, когда нажата ЛКМ
+            if (e.MouseDevice.LeftButton == MouseButtonState.Pressed)
+            {
+                p1 = e.GetPosition(canvas.Children[0]);
+                canvas.Children.Add(new Line { X1 = p1.X, Y1 = p1.Y, X2 = noDraw ? p1.X : p2.X, Y2 = noDraw ? p1.Y : p2.Y, StrokeStartLineCap = PenLineCap.Round, StrokeEndLineCap = PenLineCap.Round, StrokeThickness = 4, Stroke = Brushes.Black });
+                p2 = p1;
+                noDraw = false;
+            }
+        }
+
+        bool noDraw = true;
+        void Canvas_MouseLeftButtonUp(object sender, MouseButtonEventArgs e) => noDraw = true;//Отпустили ЛКМ - перестаём рисовать
+
+        void FindeInMemory_Click(object sender, RoutedEventArgs e)
+        {//Нажата кнопка распознать
+            if (SelectTypeRecognitiong.SelectedIndex == 0)
+            {//Через файл
+                if (selectedFile.Length == 0) { MessageBox.Show("Укажите файл для распознавания"); return; }
+
+            }
+            else if (SelectTypeRecognitiong.SelectedIndex == 1)
+            {//Рисуем
+                RenderTargetBitmap rtb = new RenderTargetBitmap((int)canvas.RenderSize.Width, (int)canvas.RenderSize.Height, 96d, 96d, PixelFormats.Default);
+                rtb.Render(canvas);
+
+                var crop = new CroppedBitmap(rtb, new Int32Rect(0, 0, (int)canvas.Width, (int)canvas.Height));
+
+                BitmapEncoder pngEncoder = new PngBitmapEncoder();
+                pngEncoder.Frames.Add(BitmapFrame.Create(crop));
+
+                using (var fs = System.IO.File.OpenWrite("file.png")) { pngEncoder.Save(fs); }
+
+
+            }
+        }
+        string selectedFile = "";
+        void SelectFile_Click(object sender, RoutedEventArgs e)
+        {//Выбрать картинку для распознавания
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == true)
+            { filePath.Content = selectedFile = ofd.FileName; }
+        }
+
+        void Canvas_MouseRightButtonDown(object sender, MouseButtonEventArgs e) { while (canvas.Children.Count > 1) canvas.Children.RemoveAt(canvas.Children.Count - 1); }//Очищает поле для рисования
     }
 }
---
+++ Neuronus/Neiron.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+
+namespace Neuronus
+{
+    class Neiron
+    {
+        double[,] weight = new double[200, 200];
+        public char myChar { get { return myChar; } private set { myChar = value; } }
+        public Neiron(char inputChar) => myChar = inputChar;
+
+        public void Train(byte[,] input)
+        {
+            //TO DO
+        }
+
+        public void Recognize(byte[,] input)
+        {
+            //TO DO
+        }
+    }
+}
