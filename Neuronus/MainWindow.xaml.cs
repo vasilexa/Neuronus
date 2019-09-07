df21f83b3753ab3ff1ae6c2fca7eea07f76648e2
Medoo48 <artem_1889@mail.ru>
2019-09-07 16:51:15 +0300

    Добавил проект

---
+++ Neuronus/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Navigation;
+using System.Collections.Generic;
+using System.Windows.Media.Imaging;
+
+
+namespace Neuronus
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        { InitializeComponent(); }
+
+        void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) => DragMove();
+    }
+}
